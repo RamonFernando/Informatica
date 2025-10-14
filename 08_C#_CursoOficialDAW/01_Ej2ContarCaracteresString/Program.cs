b147bb64fb43bfa89e7d48cd1aa85de29daba871
RamonFernando <ramonrfpa88@gmail.com>
2025-10-14 16:40:58 +0200

    Ejercicio 1 DAW C#

---
+++ 08_C#_CursoOficialDAW/01_Ej2ContarCaracteresString/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_Ej2ContarCaracteresString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduzca una frase: ");
+            string frase = Console.ReadLine();
+            while (frase.Length == 0 || frase == null)
+            {   
+                Console.WriteLine("No has introducido ninguna frase, intentalo de nuevo: ");
+                frase = Console.ReadLine();
+            }
+            
+            Console.WriteLine("La frase tiene: " + frase.Length + " caracteres");
+            
+                
+            Console.ReadKey();
+        }
+    }
+}
