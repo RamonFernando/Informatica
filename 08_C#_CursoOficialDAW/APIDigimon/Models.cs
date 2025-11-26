1ecce21449e5ba8297299412a4b09f2021902c21
RamonFernando <ramonrfpa88@gmail.com>
2025-11-26 01:50:09 +0100

    agregando APIDigimon C#

---
+++ 08_C#_CursoOficialDAW/APIDigimon/Models.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APIDigimon
+{
+    // Nombre de la clase, propiedades de la API (Digimon)
+    public static class Models
+    {
+        public class ClassDigimon
+        {
+
+            public int Id { get; set; }
+
+            public string Name { get; set; }
+
+        }
+        public class Levels
+        {
+            public static int Id { get; set; }
+            public static int Level { get; set; }
+
+        }
+        public class Types
+        {
+            public static int Id { get; set; }
+            public static string Type { get; set; }
+        }
+
+        // Raíz del JSON que devuelve la API: info + results
+        public class ClassRoot
+        {
+            public List<FavoriteItemList> FavoriteList { get; set; } = new List<FavoriteItemList>();
+        }
+
+
+        // Modelo de la lista de favoritos
+        public class FavoriteItemList
+        {
+            public int Id { get; set; }
+
+            public string Name { get; set; }
+
+            public string Type { get; set; }
+
+            public string Level { get; set; }
+
+        }
+    }
+}
