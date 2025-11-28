63e624eca9a3ee6651cf72d4ed6e8da465ada9f0
RamonFernando <ramonrfpa88@gmail.com>
2025-11-28 17:26:11 +0100

    agregando APIPlaceholderAPI C#

---
+++ 08_C#_CursoOficialDAW/APIPlaceHolderAPI/Models/Models.cs
@@ -0,0 +1,45 @@
+﻿using Newtonsoft.Json;
+
+namespace APIPlaceHolderAPI
+{
+    public class Models
+    {
+
+        // Nombre de la clase, propiedades de la API
+
+        // 1. Clase Digimon (objeto modelo)
+        public class ClassTask
+        {
+            [JsonProperty("userId")]
+            public int UserId { get; set; }
+
+            [JsonProperty("id")]
+            public int Id { get; set; }
+
+            [JsonProperty("title")]
+            public string Title { get; set; }
+
+            [JsonProperty("completed")]
+            public bool Completed { get; set; }
+
+        }
+
+        // 2. Modelo de la lista de favoritos
+        public class FavoriteItemList // *poner atributos del metodo AddToFavoriteList y ShowFavoriteList*
+        {
+            [JsonProperty("userId")]
+            public int UserId { get; set; }
+
+            [JsonProperty("id")]
+            public int Id { get; set; }
+
+            [JsonProperty("title")]
+            public string Title { get; set; }
+
+            [JsonProperty("completed")]
+            public bool Completed { get; set; }
+
+        }
+    } // Fin de la clase
+}
+
---
+++ 08_C#_CursoOficialDAW/APIPlaceHolderAPI/Services/APILoadJson.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+using static APIPlaceHolderAPI.Models;
+using static APIPlaceHolderAPI.Program;
+using static APIPlaceHolderAPI.APIControllers;
+
+namespace APIPlaceHolderAPI
+{
+    internal class APILoadJson
+    {
+        public static void APILoadFavoriteList()
+        {
+            try
+            {
+                string filePth = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "favoriteListHolder.json");
+
+                // Comprobamos si el archivo existe
+                if (!File.Exists(filePth))
+                {
+                    Console.WriteLine("No se ha encontrado el archivo de favoriteList.json, se creara uno nuevo.");
+                    PrintWaitForPressKey();
+                    return;
+                }
+
+                // Leemos el archivo
+                string json = File.ReadAllText(filePth);
+
+                favoriteList = JsonConvert.DeserializeObject<List<FavoriteItemList>>(json); // Convierte el JSON en una lista
+
+            }
+            catch (Exception ex)
+            {
+                HandlerException(ex);
+                PrintWaitForPressKey();
+            }
+        } // APILoadFavoriteList
+    } // APILoadJson
+}
