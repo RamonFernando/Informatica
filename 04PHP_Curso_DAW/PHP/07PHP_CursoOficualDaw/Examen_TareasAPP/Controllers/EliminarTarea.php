cc3d4eb8ed582872f9d0b5e8a9dd809cafefbd89
RamonFernando <ramonrfpa88@gmail.com>
2025-12-16 20:00:20 +0100

    agregando eliminar a Examen_TareasAPP

--- 04PHP_Curso_DAW/PHP/07PHP_CursoOficualDaw/Examen_TareasAPP/Controllers/EliminarTarea.php
+++ 04PHP_Curso_DAW/PHP/07PHP_CursoOficualDaw/Examen_TareasAPP/Controllers/EliminarTarea.php
@@ -1,6 +1,14 @@
 <?php
-    
-    function eliminarTarea($tareas, $indice) {
-        
+    function eliminarTarea($tareas, $id) {
+        foreach ($tareas as $index => $tarea) {
+            if($tarea->getId() === $id ){
+                unset($tareas[$index]);
+                $tareas = array_values($tareas); // Reindexar el array
+                echo "Tarea con ID '$id' eliminada correctamente.";
+                return $tareas;
+            }
+        }
+        echo "Tarea con ID '$id' no encontrada.";
+        return $tareas;
     }
 ?>
